31221ecd5ed0fdd50d5b64cd95e388b84218749b
sabianathallah <sabian.athallah05@gmail.com>
2026-01-01 17:40:41 +0700

    chore: register only create by admin

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,14 +3,15 @@ const router = express.Router()
 
 const attandanceRouter = require('./attendance')
 
+const isAdmin = require("../middlewares/authorization");
 const authentication = require('../middlewares/authentication')
-const authorization = require('../middlewares/authorization')
 const errorHandler = require('../middlewares/errorHandler')
 
 const LoginController = require('../controllers/loginController')
 
+
 router.post('/login', LoginController.login)
-router.post('/register', LoginController.register)
+router.post('/register', authentication, isAdmin, LoginController.register)
 
 router.use(authentication)
 router.use('/attendances', attandanceRouter)
